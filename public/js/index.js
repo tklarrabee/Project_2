4dc8edbf400f4131a971be2716d157207a88b422
Tyler Larrabee <tyler.k.larrabee@gmail.com>
2019-06-25 21:19:53 -0700

    record deletions schema update

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,76 @@ var $exampleDescription = $("#example-description");
 var $submitBtn = $("#submit");
 var $exampleList = $("#example-list");
 
+var str=$("#area").val();
+// get information
+var t=str.split("\n");
+for (var i=0;i<t.length;i++)
+{
+  var arr=t[i].split("\t");
+  document.write("'<row dc= '" + arr[0] + "' al='" + arr[1] + "' msg='" + arr[2] + "' />'")
+}
+
+$.fn.getCursorPosition = function() {
+  var el = $(this).get(0);
+  var pos = 0;
+  var posEnd = 0;
+  if('selectionStart' in el) {
+      pos = el.selectionStart;
+      posEnd = el.selectionEnd;
+  } else if('selection' in document) {
+      el.focus();
+      var Sel = document.selection.createRange();
+      var SelLength = document.selection.createRange().text.length;
+      Sel.moveStart('character', -el.value.length);
+      pos = Sel.text.length - SelLength;
+      posEnd = Sel.text.length;
+  }
+  // return both selection start and end;
+  return [pos, posEnd];
+};
+// Detect which key was deleted
+// make it so that it will only log on a delete
+$('#area').keydown(function (e) {
+  var position = $(this).getCursorPosition();
+  var deleted = '';
+  var val = $(this).val();
+  if (e.which == 8) {
+      if (position[0] == position[1]) {
+          if (position[0] == 0)
+              deleted = '';
+          else
+              deleted = val.substr(position[0] - 1, 1);
+      }
+      else {
+          deleted = val.substring(position[0], position[1]);
+      }
+  }
+  else if (e.which == 46) {
+      var val = $(this).val();
+      if (position[0] == position[1]) {
+
+          if (position[0] === val.length)
+              deleted = '';
+          else
+              deleted = val.substr(position[0], 1);
+      }
+      else {
+          deleted = val.substring(position[0], position[1]);
+      }
+  }
+  console.log(deleted);
+  // Now you can test the deleted character(s) here
+});
+
+function Element(body, type, date, urgent, user){
+  this.body = body;
+  this.type = type;
+  this.date = date;
+  this.urgent = urgent;
+  this.user = user;
+}
+
+
 // The API object contains methods for each kind of request we'll make
 var API = {
   saveExample: function(example) {

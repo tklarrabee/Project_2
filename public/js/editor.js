2f74ae573434adcfe5f42eb4cbb04a5c4529dd8e
Tyler Larrabee <tyler.k.larrabee@gmail.com>
2019-06-29 12:46:17 -0700

    hotkey bullet point

---
+++ public/js/editor.js
@@ -0,0 +1,96 @@
+/* eslint-disable no-undef */
+const task = '•'
+const event = '&#9702'
+const note = '&#8259'
+
+document.write(task, event, note);
+
+function Element (body, type, date, urgent, user) {
+  this.body = body
+  this.type = type
+  this.date = date
+  this.urgent = urgent
+  this.user = user
+}
+// on submit do this
+var str = $('#area').val()
+// get information
+var t = str.split('\n')
+for (var i = 0; i < t.length; i++) {
+  var arr = t[i].split('\t')
+  document.write("'<row dc= '" + arr[0] + "' al='" + arr[1] + "' msg='" + arr[2] + "' />'")
+}
+
+$(function () {
+  $('#button').on('click', function () {
+    var text = $('#text')
+    text.val(text.val() + ' after clicking')
+  })
+})
+
+$.fn.getCursorPosition = function () {
+  var el = $(this).get(0)
+  var pos = 0
+  var posEnd = 0
+  if ('selectionStart' in el) {
+    pos = el.selectionStart
+    posEnd = el.selectionEnd
+  } else if ('selection' in document) {
+    el.focus()
+    var Sel = document.selection.createRange()
+    var SelLength = document.selection.createRange().text.length
+    Sel.moveStart('character', -el.value.length)
+    pos = Sel.text.length - SelLength
+    posEnd = Sel.text.length
+  }
+  // return both selection start and end;
+  return [pos, posEnd]
+}
+// Detect which key was deleted
+// make it so that it will only log on a delete
+$('#area').keydown(function (e) {
+  var position = $(this).getCursorPosition()
+  var deleted = ''
+  var val = $(this).val()
+  if (e.which == 8) {
+    if (position[0] == position[1]) {
+      if (position[0] == 0) { deleted = '' } else { deleted = val.substr(position[0] - 1, 1) }
+    } else {
+      deleted = val.substring(position[0], position[1])
+    }
+  } else if (e.which == 46) {
+    var val = $(this).val()
+    if (position[0] == position[1]) {
+      if (position[0] === val.length) { deleted = '' } else { deleted = val.substr(position[0], 1) }
+    } else {
+      deleted = val.substring(position[0], position[1])
+    }
+  }
+  if (deleted !== '') {
+    console.log(deleted)
+  }
+
+  // Now you can test the deleted character(s) here
+})
+
+$('#area').keydown(function (e) {
+  // console.log('event ' + e.ctrlKey)
+  if (e.ctrlKey) {
+    $('#area').keydown(function (e) {
+      if (e.which === 190) {
+        // let val = $(this).val()
+        let end = str.length
+        let val = str
+        let position = $(this).getCursorPosition()
+        let halfOne = str.substring(0, position[0])
+        let halfTwo = str.substring(position[1], end)
+        val = halfOne + ' ' + task + ' ' + halfTwo
+        $(this).val(val)
+        str = $(this).val()
+        // $(this).val(val)
+        console.log(halfOne, halfTwo)
+      }
+    })
+  }
+})
+// Constructor for new tasks, events, and notes.
--- public/js/index.js
+++ public/js/index.js
@@ -1,169 +1,164 @@
 // Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+var $exampleText = $('#example-text')
+var $exampleDescription = $('#example-description')
+var $submitBtn = $('#submit')
+var $exampleList = $('#example-list')
 
-var str=$("#area").val();
+var str = $('#area').val()
 // get information
-var t=str.split("\n");
-for (var i=0;i<t.length;i++)
-{
-  var arr=t[i].split("\t");
+var t = str.split('\n')
+for (var i = 0; i < t.length; i++) {
+  var arr = t[i].split('\t')
   document.write("'<row dc= '" + arr[0] + "' al='" + arr[1] + "' msg='" + arr[2] + "' />'")
 }
 
-$.fn.getCursorPosition = function() {
-  var el = $(this).get(0);
-  var pos = 0;
-  var posEnd = 0;
-  if('selectionStart' in el) {
-      pos = el.selectionStart;
-      posEnd = el.selectionEnd;
-  } else if('selection' in document) {
-      el.focus();
-      var Sel = document.selection.createRange();
-      var SelLength = document.selection.createRange().text.length;
-      Sel.moveStart('character', -el.value.length);
-      pos = Sel.text.length - SelLength;
-      posEnd = Sel.text.length;
+$(function () {
+  $('#button').on('click', function () {
+    var text = $('#text');
+    text.val(text.val() + ' after clicking');
+  })
+})
+
+$.fn.getCursorPosition = function () {
+  var el = $(this).get(0)
+  var pos = 0
+  var posEnd = 0
+  if ('selectionStart' in el) {
+    pos = el.selectionStart
+    posEnd = el.selectionEnd
+  } else if ('selection' in document) {
+    el.focus()
+    var Sel = document.selection.createRange()
+    var SelLength = document.selection.createRange().text.length
+    Sel.moveStart('character', -el.value.length)
+    pos = Sel.text.length - SelLength
+    posEnd = Sel.text.length
   }
   // return both selection start and end;
-  return [pos, posEnd];
-};
+  return [pos, posEnd]
+}
 // Detect which key was deleted
 // make it so that it will only log on a delete
 $('#area').keydown(function (e) {
-  var position = $(this).getCursorPosition();
-  var deleted = '';
-  var val = $(this).val();
+  var position = $(this).getCursorPosition()
+  var deleted = ''
+  var val = $(this).val()
   if (e.which == 8) {
-      if (position[0] == position[1]) {
-          if (position[0] == 0)
-              deleted = '';
-          else
-              deleted = val.substr(position[0] - 1, 1);
-      }
-      else {
-          deleted = val.substring(position[0], position[1]);
-      }
+    if (position[0] == position[1]) {
+      if (position[0] == 0) { deleted = '' } else { deleted = val.substr(position[0] - 1, 1) }
+    } else {
+      deleted = val.substring(position[0], position[1])
+    }
+  } else if (e.which == 46) {
+    var val = $(this).val()
+    if (position[0] == position[1]) {
+      if (position[0] === val.length) { deleted = '' } else { deleted = val.substr(position[0], 1) }
+    } else {
+      deleted = val.substring(position[0], position[1])
+    }
   }
-  else if (e.which == 46) {
-      var val = $(this).val();
-      if (position[0] == position[1]) {
-
-          if (position[0] === val.length)
-              deleted = '';
-          else
-              deleted = val.substr(position[0], 1);
-      }
-      else {
-          deleted = val.substring(position[0], position[1]);
-      }
-  }
-  console.log(deleted);
+  console.log(deleted)
   // Now you can test the deleted character(s) here
-});
+})
 
-function Element(body, type, date, urgent, user){
-  this.body = body;
-  this.type = type;
-  this.date = date;
-  this.urgent = urgent;
-  this.user = user;
+function Element(body, type, date, urgent, user) {
+  this.body = body
+  this.type = type
+  this.date = date
+  this.urgent = urgent
+  this.user = user
 }
-
 
 // The API object contains methods for each kind of request we'll make
 var API = {
-  saveExample: function(example) {
+  saveExample: function (example) {
     return $.ajax({
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       },
-      type: "POST",
-      url: "api/examples",
+      type: 'POST',
+      url: 'api/examples',
       data: JSON.stringify(example)
-    });
+    })
   },
-  getExamples: function() {
+  getExamples: function () {
     return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
+      url: 'api/examples',
+      type: 'GET'
+    })
   },
-  deleteExample: function(id) {
+  deleteExample: function (id) {
     return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
+      url: 'api/examples/' + id,
+      type: 'DELETE'
+    })
   }
-};
+}
 
 // refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
+var refreshExamples = function () {
+  API.getExamples().then(function (data) {
+    var $examples = data.map(function (example) {
+      var $a = $('<a>')
         .text(example.text)
-        .attr("href", "/example/" + example.id);
+        .attr('href', '/example/' + example.id)
 
-      var $li = $("<li>")
+      var $li = $('<li>')
         .attr({
-          class: "list-group-item",
-          "data-id": example.id
+          class: 'list-group-item',
+          'data-id': example.id
         })
-        .append($a);
+        .append($a)
 
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
+      var $button = $('<button>')
+        .addClass('btn btn-danger float-right delete')
+        .text('ｘ')
 
-      $li.append($button);
+      $li.append($button)
 
-      return $li;
-    });
+      return $li
+    })
 
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
+    $exampleList.empty()
+    $exampleList.append($examples)
+  })
+}
 
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
+var handleFormSubmit = function (event) {
+  event.preventDefault()
 
   var example = {
     text: $exampleText.val().trim(),
     description: $exampleDescription.val().trim()
-  };
-
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
   }
 
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
+  if (!(example.text && example.description)) {
+    alert('You must enter an example text and description!')
+    return
+  }
 
-  $exampleText.val("");
-  $exampleDescription.val("");
-};
+  API.saveExample(example).then(function () {
+    refreshExamples()
+  })
+
+  $exampleText.val('')
+  $exampleDescription.val('')
+}
 
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
+var handleDeleteBtnClick = function () {
   var idToDelete = $(this)
     .parent()
-    .attr("data-id");
+    .attr('data-id')
 
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
+  API.deleteExample(idToDelete).then(function () {
+    refreshExamples()
+  })
+}
 
 // Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+$submitBtn.on('click', handleFormSubmit)
+$exampleList.on('click', '.delete', handleDeleteBtnClick)

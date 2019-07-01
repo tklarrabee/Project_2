00592c6ed416b38d330dbeb93c8b92668688ea33
smprov22 <smprovence22@gmail.com>
2019-06-30 22:55:51 -0700

    styling changes

--- app/models/user.js
+++ app/models/user.js
@@ -1,37 +1,36 @@
-module.exports = function(sequelize, Sequelize) {
- 
-    var User = sequelize.define('user', {
- 
-        id: {
-            autoIncrement: true,
-            primaryKey: true,
-            type: Sequelize.INTEGER
-        },
- 
-        firstname: {
-            type: Sequelize.STRING,
-            notEmpty: true
-        },
- 
-        lastname: {
-            type: Sequelize.STRING,
-            notEmpty: true
-        },
- 
-        email: {
-            type: Sequelize.STRING,
-            validate: {
-                isEmail: true
-            }
-        },
- 
-        password: {
-            type: Sequelize.STRING,
-            allowNull: false
-        }, 
- 
-    });
- 
-    return User;
- 
+/* eslint-disable comma-dangle */
+module.exports = function (sequelize, Sequelize) {
+  var User = sequelize.define('user', {
+
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+
+    firstname: {
+      type: Sequelize.STRING,
+      notEmpty: true
+    },
+
+    lastname: {
+      type: Sequelize.STRING,
+      notEmpty: true
+    },
+
+    email: {
+      type: Sequelize.STRING,
+      validate: {
+        isEmail: true
+      }
+    },
+
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+
+  })
+
+  return User
 }

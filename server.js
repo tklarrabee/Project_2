4ab1664ba6c1665e19fc343495928bc90ac443d4
One Punch <bodnguye@gmail.com>
2019-06-30 19:43:29 -0700

    everything is in app folder/ changed up server.js

--- server.js
+++ server.js
@@ -1,58 +1,102 @@
-require("dotenv").config();
-var express = require("express");
-// var exphbs = require("express-handlebars");
-var expressLayouts = require('express-ejs-layouts');
-
-var db = require('./models')
-
+var express = require('express')
 var app = express()
-var PORT = process.env.PORT || 3000
+const expressLayouts = require('express-ejs-layouts'); // dont need if handlbars work
+var passport = require('passport')
+var session = require('express-session')
+var bodyParser = require('body-parser')
+var env = require('dotenv').config()
+var exphbs = require('express-handlebars')
 
-// EJS.
-app.use(expressLayouts);
-app.set('view engine', 'ejs');
+// var db = require('./models') *** PROBABLY DELETE ALSO
 
-// Middleware
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(express.static('public'))
+// For BodyParser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static("public"));
 
-// Routes
-require("./routes/apiRoutes")(app);
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
+// Middleware ** USING BODY PARSER...might delete this
+// app.use(express.urlencoded({ extended: false })) 
+// app.use(express.json())
+// app.use(express.static('public'))
 
-// Handlebars
-// app.engine(
-//   "handlebars",
-//   exphbs({
-//     defaultLayout: "main"
-//   })
-// );
-// app.set("view engine", "handlebars");
+// For Passport
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
-// Routes
-require('./routes/apiRoutes')(app)
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
 
-var syncOptions = { force: false }
+//  EJS                DELETE IF Handlebars works
+// app.use(expressLayouts);
+// app.set('view engine', 'ejs');
 
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === 'test') {
-  syncOptions.force = true
-}
+// //routes
+// app.use('/', require('./app/routes/index'));
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function () {
-  app.listen(PORT, function () {
-    console.log(
-      '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
-      PORT,
-      PORT
-    )
-  })
-})
+// For Handlebars
+app.set('views', './app/views')
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+ 
 
-module.exports = app
+app.get('/', function(req, res) {
+ 
+    res.render('welcome');
+ 
+});
+
+//Models
+var models = require("./app/models");
+ 
+//Routes
+var authRoute = require('./app/routes/auth.js')(app,passport);
+ 
+ 
+//load passport strategies
+require('./app/config/passport/passport.js')(passport, models.user);
+ 
+ 
+//Sync Database
+models.sequelize.sync().then(function() {
+ 
+    console.log('Nice! Database looks fine')
+ 
+ 
+}).catch(function(err) {
+ 
+    console.log(err, "Something went wrong with the Database Update!")
+ 
+});
+ 
+ 
+app.listen(5000, function(err) {
+ 
+    if (!err)
+ 
+        console.log("Site is live");
+         
+    else console.log(err)
+ 
+});
+
+// Routes                                        *** Might delete if handlebars works
+// require("./routes/apiRoutes")(app);
+// app.use('/', require('./routes/index'));
+// app.use('/users', require('./routes/users'));
+
+// var syncOptions = { force: false }                        // DO WE NEED THIS???
+
+// If running a test, set syncOptions.force to true       OR THIS??
+// clearing the `testdb`   "comment"
+
+// if (process.env.NODE_ENV === 'test') {
+//   syncOptions.force = true
+// }
+
+
+// module.exports = app   // DONT THINK WE NEED THIS?

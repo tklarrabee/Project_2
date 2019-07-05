11e47816010de239a784ee563a8f26c87f3a8e8c
smprov22 <smprovence22@gmail.com>
2019-07-05 08:43:01 -0700

    another merge fix

--- app/models/Tasks.js
+++ app/models/Tasks.js
@@ -1,14 +1,10 @@
 module.exports = function (sequelize, DataTypes) {
-<<<<<<< HEAD
   var Tasks = sequelize.define('Tasks', {
-=======
-  var Tasks = sequelize.define("Tasks", {
     id: {
       autoIncrement: true,
       primaryKey: true,
       type: DataTypes.INTEGER
-  },
->>>>>>> 06cef6ba883408bf88577b90442899ff0616c95e
+    },
     type: DataTypes.STRING,
     body: {
       type: DataTypes.TEXT,

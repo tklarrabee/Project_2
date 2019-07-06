f2086d033f45550e2fcb5d9077ab19ee2f6c9b66
smprov22 <smprovence22@gmail.com>
2019-07-05 22:48:18 -0700

    redo pull request

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(express.static('public'))
 
-// For Passport
+// For Passport.js
 app.use(session({
   secret: 'keyboard cat',
   resave: true,
